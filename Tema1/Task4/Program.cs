16f28355c3c89aaaa8a6bc74edde266fbab6f38c
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 12:42:37 +0300

    Сделано задание 4

---
+++ Tema1/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите курс доллара: ");
+            double dollarER = double.Parse(Console.ReadLine());
+
+            for (int d = 5; d <= 500; d += 5)
+            {
+                double dollarsToByn = d * dollarER;
+
+                Console.WriteLine($"{d} долларов = {dollarsToByn} белорусских рублей");
+            }
+        }
+    }
+}
