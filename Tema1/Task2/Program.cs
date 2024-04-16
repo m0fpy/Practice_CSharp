39f398115f3957a81472d4ba2f761922aede51b6
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 12:30:36 +0300

    Сделано задание 2

---
+++ Tema1/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону a: ");
+            double sideA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону b: ");
+            double sideB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону c: ");
+            double sideC = double.Parse(Console.ReadLine());
+
+            if(CheckForExistence(sideA, sideB, sideC)) 
+            { 
+                Console.WriteLine("Треугольник может существовать");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не может существовать");
+            }
+        }
+
+        public static bool CheckForExistence(double sideA, double sideB, double sideC)
+        {
+            if(sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                return false;
+            }
+
+            if((sideA + sideB > sideC) || (sideA + sideC > sideB) || (sideB + sideC > sideB)) 
+            {
+                return false;            
+            }
+
+
+
+            return true;
+        }
+    }
+}
