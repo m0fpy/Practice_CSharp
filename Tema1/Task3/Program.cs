b7610826b4f40e2a7c3cc6b4edc11cb505891ebf
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 12:35:59 +0300

    Сделано задание 3

---
+++ Tema1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер дня месяца:");
+            int dayOfMonth = Convert.ToInt32(Console.ReadLine());
+
+            int remainingDays = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - dayOfMonth;
+
+            Console.WriteLine($"Количество дней оставшихся до конца месяца: {remainingDays}");
+        }
+    }
+}
