845468a2562bf51ffde880ab6895fd4550f9e590
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 14:11:42 +0300

    Сделано задание 5

--- Tema1/Task5/Program.cs
+++ Tema1/Task5/Program.cs
@@ -7,6 +7,12 @@
             Console.WriteLine("Введите стоимость 1 кг. конфет: ");
             int cost = int.Parse(Console.ReadLine());
 
+            if (cost < 1 || cost > 100)
+            {
+                Console.WriteLine("Некорректные данные");
+                return;
+            }
+
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine($"Стоимсоть {i} кг. конфет = {cost * i}");
