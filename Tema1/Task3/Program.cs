d017230b51cfba485c30482908bb90d26e2761c9
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 13:37:36 +0300

    Сделано задание 3

--- Tema1/Task3/Program.cs
+++ Tema1/Task3/Program.cs
@@ -5,11 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Введите порядковый номер дня месяца:");
-            int dayOfMonth = Convert.ToInt32(Console.ReadLine());
+            int dayOfMonth = int.Parse(Console.ReadLine());
 
-            int remainingDays = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - dayOfMonth;
+            Console.WriteLine("Введите порядковый номер месяца:");
+            int month = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Количество дней оставшихся до конца месяца: {remainingDays}");
+            string remainingDays = CalculateRemainingDays(month, dayOfMonth);
+
+            Console.WriteLine(remainingDays);
+        }
+
+        static string CalculateRemainingDays(int month, int dayOfMonth )
+        {
+            return month switch
+            {
+                1 or 3 or 5 or 7 or 8 or 10 or 12 => dayOfMonth > 31 ? "Неправильный номер дня" : $"До конца месяца осталось {31 - dayOfMonth} дней",
+                2 => dayOfMonth > 28 ? "Неправильный номер дня" : $"До конца месяца осталось {28 - dayOfMonth} дней",
+                4 or 6 or 9 or 11 => dayOfMonth > 30 ? "Неправильный номер дня" : $"До конца месяца осталось {30 - dayOfMonth} дней",
+                _ => "Неправильный номер месяца"
+            };
         }
     }
 }
