5ae028a36defb07ab835686494bad65f3449ce32
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 13:11:34 +0300

    Сделано задание 7

---
+++ Tema1/Task7/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double[] numbers = new double[rnd.Next(5, 30)];
+            double maxAbsValue = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(-50, 50) * rnd.NextDouble();
+            }
+
+            foreach(double number in numbers)
+            {
+                if (Math.Abs(number) > maxAbsValue)
+                {
+                    maxAbsValue = Math.Abs(number);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+            Console.WriteLine($"Максимальный модуль = {maxAbsValue:F2}");
+            
+        }
+    }
+}
