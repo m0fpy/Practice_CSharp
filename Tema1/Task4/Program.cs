b2c2d0852a379409fd3f9b9aeec93afef231f761
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 12:50:30 +0300

    Сделано задание 4

--- Tema1/Task4/Program.cs
+++ Tema1/Task4/Program.cs
@@ -7,12 +7,32 @@
             Console.WriteLine("Введите курс доллара: ");
             double dollarER = double.Parse(Console.ReadLine());
 
+            //Решение через for
             for (int d = 5; d <= 500; d += 5)
             {
                 double dollarsToByn = d * dollarER;
 
                 Console.WriteLine($"{d} долларов = {dollarsToByn} белорусских рублей");
             }
+
+            int dollarCount = 5;
+            //Решение через do/while
+            do
+            {
+                double dollarsToByn = dollarCount * dollarER;
+                Console.WriteLine($"{dollarCount} долларов = {dollarsToByn} белорусских рублей");
+                dollarCount += 5;
+            }
+            while ( dollarCount != 500);
+
+            //Решение через while
+            while (dollarCount <= 500)
+            {
+                double dollarsToByn = dollarCount * dollarER;
+                Console.WriteLine($"{dollarCount} долларов = {dollarsToByn} белорусских рублей");
+                dollarCount += 5;
+            }
+
         }
     }
 }
