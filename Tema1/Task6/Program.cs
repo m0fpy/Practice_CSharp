c558bb6e50a4ee7ff0349ab919caa6160bc3eb62
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 12:57:52 +0300

    Сделано задание

---
+++ Tema1/Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double ARG_A = 0;
+            const double ARG_B = Math.PI / 2;
+            const double ARG_M = 20;
+
+            double stepH = (ARG_B - ARG_A) / ARG_M;
+
+            for (double x = ARG_A; x <= ARG_B; x += stepH)
+            {
+
+                Console.WriteLine($"y = {(Math.Sin(x) - Math.Cos(x)):F2}, при х = {x:F2}");
+            }
+        }
+    }
+}
