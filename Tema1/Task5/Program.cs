4039f3a1fece329c0a5ad52cd9099078c4e49fe2
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-16 12:51:09 +0300

    Сделано задание 5

---
+++ Tema1/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стоимость 1 кг. конфет: ");
+
+            int cost = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Стоимсоть {i} кг. конфет = {cost * i}");
+            }
+        }
+    }
+}
