bb94a90ef0ccb54f2c701a65b96603849353f53e
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-17 13:07:43 +0300

    Сделано задание 1

---
+++ Tema2/Task1/ClassA.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    internal class ClassA
+    {
+        int paramA;
+        int paramB;
+
+        public ClassA(int paramA, int paramB)
+        {
+            this.paramA = paramA;
+            this.paramB = paramB;
+        }
+
+        public double CalculateFunction(int paramA, int paramB)
+        {
+            return Math.Round((Math.Sin(paramB) + 4) / (2 * paramA), 3);
+        }
+
+        public int CaculateSumSquare(int paramA, int paramB)
+        {
+            return (int)Math.Pow((paramA + paramB), 2);
+        }
+
+    }
+}
---
+++ Tema2/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA classAObject = new ClassA(5, 6);
+
+            Console.WriteLine($"Функция = {classAObject.CalculateFunction(5, 6)}");
+            Console.WriteLine($"Квадрат суммы = {classAObject.CaculateSumSquare(5, 6)}");
+        }
+    }
+}
