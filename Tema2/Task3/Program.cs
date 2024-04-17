e1d78995299115b53226b73e105ee5f7d770dc47
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-17 13:08:11 +0300

    Сделано задание 3

---
+++ Tema2/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation eq1 = new QuadraticEquation(1, 2, 1);
+            QuadraticEquation eq2 = new QuadraticEquation(2, -1, -3);
+
+            Console.WriteLine(eq1.Display());
+            Console.WriteLine(eq2.Display());
+
+            Console.WriteLine(eq2.GetResultForArgument(5));
+
+            QuadraticEquation sum = eq1 + eq2;
+            Console.WriteLine("Сумма многочленов:");
+            Console.WriteLine(sum.Display());
+
+            QuadraticEquation diff = eq1 - eq2;
+            Console.WriteLine("\nРазность многочленов:");
+            Console.WriteLine(diff.Display());
+
+            QuadraticEquation product = eq1 * eq2;
+            Console.WriteLine("\nПроизведение многочленов:");
+            Console.WriteLine(product.Display());
+
+            Console.WriteLine(eq1.ToString());
+        }
+    }
+}
---
+++ Tema2/Task3/QuadraticEquation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class QuadraticEquation
+    {
+        private double argA;
+        private double argB;
+        private double argC;
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            argA = a;
+            argB = b;
+            argC = c;
+        }
+
+        public double GetResultForArgument(double x)
+        {
+            return argA * Math.Pow(x, 2) + argB * x + argC;
+        }
+
+        public static QuadraticEquation operator +(QuadraticEquation eq1, QuadraticEquation eq2)
+        {
+            double newA = eq1.argA + eq2.argA;
+            double newB = eq1.argB + eq2.argB;
+            double newC = eq1.argC + eq2.argC;
+            return new QuadraticEquation(newA, newB, newC);
+        }
+
+        public static QuadraticEquation operator -(QuadraticEquation eq1, QuadraticEquation eq2)
+        {
+            double newA = eq1.argA - eq2.argA;
+            double newB = eq1.argB - eq2.argB;
+            double newC = eq1.argC - eq2.argC;
+            return new QuadraticEquation(newA, newB, newC);
+        }
+
+        public static QuadraticEquation operator *(QuadraticEquation eq1, QuadraticEquation eq2)
+        {
+            double newA = eq1.argA * eq2.argA;
+            double newB = eq1.argA * eq2.argB + eq1.argB * eq2.argA;
+            double newC = eq1.argA * eq2.argC + eq1.argB * eq2.argB + eq1.argC * eq2.argA;
+            return new QuadraticEquation(newA, newB, newC);
+        }
+
+        public string Display()
+        {
+            return $"{argA}x^2 + {argB}x + {argC} = 0";
+        }
+
+        public override string ToString()
+        {
+            return $"Аргумент а - {argA}, аргумент b - {argB}, аргумент c - {argC}";
+        }
+    }
+}
