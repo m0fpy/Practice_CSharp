ad30f08ae6a03ba6361e3e4380e25e4ee8726253
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-18 09:48:37 +0300

    Сделано задание 2

---
+++ Tema3/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение А: ");
+            double numA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение B: ");
+            double numB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение C: ");
+            double numC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение D: ");
+            double numD = double.Parse(Console.ReadLine());
+
+            Swap(ref numA, ref numB);
+            Swap(ref numC, ref numD);
+            Swap(ref numB, ref numC);
+
+
+            Console.WriteLine($"Значение А: {numA}, Значение B: {numB}, Значение C: {numC}, Значение D: {numD}");
+        }
+
+        static void Swap(ref double x, ref double y)
+        {
+            (x, y) = (y, x);
+        }
+    }
+}
