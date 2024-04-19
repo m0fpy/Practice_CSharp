292d452243a2c960e5c7417f1ec7d71fff39c39f
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-19 16:01:58 +0300

    Сделано приложение А

--- PrilozhenieA/Task1/Program.cs
+++ PrilozhenieA/Task1/Program.cs
@@ -6,9 +6,12 @@
         {
             Console.Write("a = ");
             double num_a = double.Parse(Console.ReadLine());
+
             Console.Write("b = ");
             double num_b = double.Parse(Console.ReadLine());
+
             double sum = num_a + num_b;
+
             Console.WriteLine($"{num_a:F2} + {num_b:F2} = {sum:F2}");
         }
     }
--- PrilozhenieA/Task6/Program.cs
+++ PrilozhenieA/Task6/Program.cs
@@ -8,6 +8,7 @@
             {
                 double functionZ1 = (Math.Sqrt(Math.Pow((3 * argumentM), 2) - 24 * argumentM)) / ((3 * Math.Sqrt(argumentM)) - (2 / Math.Sqrt(argumentM)));
                 double functionZ2 = -Math.Sqrt(argumentM);
+
                 Console.WriteLine($"M = {argumentM:F2}, Z1 = {functionZ1:F2}, Z2 = {functionZ2:F2}");
             }
         }
