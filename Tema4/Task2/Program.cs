ada7d463c5d7f202a50f478a12331aba91bf226e
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-19 15:58:28 +0300

    Сделано задание 2

---
+++ Tema4/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Schema;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите значение аргумента х: ");
+                double argX = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Значение функции: {CalculateFunction(argX)}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static double CalculateFunction(double argX)
+        {
+            if (argX >= 1)
+            {
+                return Math.Pow(argX, 2) / (3 * argX - 9);
+            }
+            else if(argX < 1 && argX > -5) 
+            {
+                return argX - 3;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Значение аргумента должно быть в диапазоне от -5 до 1");
+            }
+        }
+    }
+}
