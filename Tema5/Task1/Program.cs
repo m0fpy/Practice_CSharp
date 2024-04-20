61941c3db84e4d9f2c95cd245539b78b43bec0d4
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-20 09:29:58 +0300

    Сделано задание 1

---
+++ Tema5/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[rnd.Next(5, 51)];
+
+            int negativeCount = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(-100, 100);
+                if (numbers[i] < 0) 
+                {
+                    negativeCount++;
+                }
+                
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Количество отрицательных чисел: {negativeCount}");
+        }
+    }
+}
