d7a09236944233ff5b6fbd7f72d0080aa3fbf31b
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-20 09:30:37 +0300

    Сделано задание 5

---
+++ Tema5/Task5/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите значение переменной N: ");
+                int numN = int.Parse(Console.ReadLine());
+
+                if (numN < 0)
+                {
+                    throw new ArgumentException("Значение N должно быть положительным.");
+                }
+
+                double functionValue = (double)CalculateFactorial(numN + 2) / CalculateFactorial(numN + 4);
+
+                Console.WriteLine($"Значение функции: {functionValue:F3}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }   
+
+        static int CalculateFactorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalculateFactorial(n - 1);
+            }
+        }
+    }
+}
