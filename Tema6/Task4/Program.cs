ad35010626b145d9332e671aa73495307dcfeb66
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-22 11:10:57 +0300

    Сделано задание 4

---
+++ Tema6/Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        const string ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ФИО пользователя (в формате 'Фамилия Имя Отчество'):");
+            string fullName = Console.ReadLine().ToUpper();
+
+            int personalityCode = CalculatePersonalityCode(fullName);
+
+            Console.WriteLine("Код личности пользователя: " + personalityCode);
+        }
+
+        static int CalculatePersonalityCode(string fullName)
+        {
+            int sum = 0;
+
+            foreach (char letter in fullName)
+            {
+                int index = ALPHABET.IndexOf(letter) + 1;
+                sum += index;
+            }
+
+            while (sum >= 10)
+            {
+                int tempSum = 0;
+                foreach (char digit in sum.ToString())
+                {
+                    tempSum += int.Parse(digit.ToString());
+                }
+                sum = tempSum;
+            }
+
+            return sum;
+        }
+    }
+}
