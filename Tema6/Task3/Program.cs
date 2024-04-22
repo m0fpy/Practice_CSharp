9bd8c4fda0d22719abb40a6c57c3b94bed4702fd
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-22 11:10:49 +0300

    Сделано задание 3

---
+++ Tema6/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string firstWordTrimmed = words.Length >= 1 ? words[0][2..] : "";
+
+            string temp = words[0];
+            words[0] = words[words.Length - 1];
+            words[words.Length - 1] = temp;
+
+            string secondAndThirdWords = words.Length >= 3 ? words[1] + words[2] : "";
+
+            string thirdWordReversed = words.Length >= 3 ? ReverseString(words[2]) : "";
+
+
+            // Вывод результатов
+            Console.WriteLine("Поменять местами первое и последнее слова: " + string.Join(" ", words));
+            Console.WriteLine("Склеить второе и третье слова: " + secondAndThirdWords);
+            Console.WriteLine("Третье слово в обратном порядке: " + thirdWordReversed);
+            Console.WriteLine("Первое слово с удаленными первыми двумя буквами: " + firstWordTrimmed);
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
