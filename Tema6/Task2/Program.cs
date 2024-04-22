32fb762944c5bdf508f0cfdbdda6571e823ed354
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-22 11:10:41 +0300

    Сделано задание 2

---
+++ Tema6/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст, состоящий только из цифр и букв:");
+            string input = Console.ReadLine();
+
+            int digitSum = CalculateDigitSum(input);
+            int textLength = input.Length;
+
+            if (digitSum == textLength)
+            {
+                Console.WriteLine("Да, сумма числовых значений цифр равна длине текста.");
+            }
+            else
+            {
+                Console.WriteLine("Нет, сумма числовых значений цифр не равна длине текста.");
+            }
+        }
+
+        static int CalculateDigitSum(string text)
+        {
+            int sum = 0;
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    sum += (int)Char.GetNumericValue(c);
+                }
+            }
+            return sum;
+        }
+    }
+}
