ff6b4116b0f4e2b80da2cf6249e8f1cb89792400
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-22 11:10:33 +0300

    Сделано задание 1

---
+++ Tema6/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string input = Console.ReadLine();
+
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine("Да, введенная строка является палиндромом.");
+            }
+            else
+            {
+                Console.WriteLine("Нет, введенная строка не является палиндромом.");
+            }
+        }
+
+        static bool IsPalindrome(string str)
+        {
+            string cleanedStr = new string(str.Where(char.IsLetter).Select(char.ToLower).ToArray());
+            return cleanedStr.SequenceEqual(cleanedStr.Reverse());
+        }
+    }
+}
