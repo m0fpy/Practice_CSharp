a346869922fd40c3d7e37c51ecf94e0548a86c74
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-23 08:57:27 +0300

    Сделано задание 2

---
+++ Tema7/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+
+            bool containsDigits = Regex.IsMatch(text, @"\d");
+
+            if (containsDigits)
+            {
+                Console.WriteLine("Текст содержит цифры.");
+            }
+            else
+            {
+                Console.WriteLine("Текст не содержит цифры.");
+            }
+        }
+    }
+}
