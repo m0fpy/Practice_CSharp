670bc3807b4141502e5d259f75c43b517d0d8550
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-23 09:11:43 +0300

    Сделано задание 4

---
+++ Tema7/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(text, @"\b\w*е\w*е\w*е\w*\b", RegexOptions.IgnoreCase);
+
+            Console.WriteLine("Слова с буквой 'e' три раза:");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
