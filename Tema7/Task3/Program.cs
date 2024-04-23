2c0c6183bdcd139e0ff3ecab4ddeff933242b4fc
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-23 08:57:37 +0300

    Сделано задание 3

---
+++ Tema7/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+
+            MatchCollection consonantMatches = Regex.Matches(text, @"[бвгджзйклмнпрстфхцчшщ]");
+
+            Console.WriteLine("Согласные буквы в тексте:");
+            foreach (Match match in consonantMatches)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+            int consonantCount = consonantMatches.Count;
+            Console.WriteLine($"Количество согласных букв: {consonantCount}");
+        }
+    }
+}
