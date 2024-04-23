e1801995328e56f84e110d16cbf60740407f0a13
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-23 08:57:19 +0300

    Сделано задание 1

---
+++ Tema7/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(text, @"(?:^\w+\b|\b\w+\b(?=\s*[\.\?!])|(?<=[\.\?!])\s*\b\w+\b)");
+            Console.WriteLine("Слова в начале и в конце предложений:");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value.Trim());
+            }
+        }
+    }
+}
