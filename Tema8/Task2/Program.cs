ead2456a18b46598aad75224f0507271a0043611
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-04-26 12:31:55 +0300

    Сделано задание 2

---
+++ Tema8/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task2
+{
+    enum Positions
+    {
+        President = 120,
+        VisePresident = 130,
+        Manager = 145,
+        Accountant = 135,
+        FrontEndDeveloper = 150,
+        BackEndDeveloper = 150
+
+    }
+
+    class Accountant
+    {
+        public bool AskForBonus(Positions worker, int hours)
+        {
+            return hours > (int)worker;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Accountant accountant = new Accountant();
+            Positions workerPosition = Positions.Manager;
+            int workerHours = 120;
+
+            Console.WriteLine(accountant.AskForBonus(workerPosition, workerHours));
+        }
+    }
+}
