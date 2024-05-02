bd7252e1c6669edcd272d732ec7064d98cda80db
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-02 11:20:38 +0300

    Сделано задание 1

---
+++ Tema12/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1
+{
+    delegate double CalcFigure(double radius);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            CalcFigure CF = Get_Length;
+            Console.WriteLine($"Длина окружности: {CF(radius):F3}");
+
+            CF = Get_Area;
+            Console.WriteLine($"Площадь круга:  {CF(radius):F3}");
+
+            CF = Get_Volume;
+            Console.WriteLine($"Объем шара: {CF(radius):F3}");
+        }
+
+        static double Get_Length(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        static double Get_Area(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        static double Get_Volume(double radius)
+        {
+            return (4.0 / 3) * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+}
