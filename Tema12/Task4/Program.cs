2fba2f49e9dfead3b6307003553ef975199e4dce
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-02 11:21:06 +0300

    Сделано задание 4

---
+++ Tema12/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        delegate int RandomDelegate();
+        delegate double AverageDelegateResults(RandomDelegate[] delegates);
+
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            RandomDelegate[] delegates =
+            [
+                () => random.Next(1000),
+                () => random.Next(2000),
+                () => random.Next(100)
+            ];
+
+            AverageDelegateResults averageDelegateResults = delegate (RandomDelegate[] delegateArray)
+            {
+                return delegateArray.Select(del => del()).Average();
+            };
+
+            double average = averageDelegateResults(delegates);
+
+            Console.WriteLine($"Среднее арифметическое: {average:F3}");
+        }
+    }
+}
