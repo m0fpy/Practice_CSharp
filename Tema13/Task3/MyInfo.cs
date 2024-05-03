d575b51a5aca96fced1d8574daac0ec363e536f8
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-03 12:35:27 +0300

    Сделано задание 3

---
+++ Tema13/Task3/MyInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace Task3
+{
+    internal class MyInfo
+    {
+        private string name;
+
+        public event EventHandler NameChanged;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnNameChanged();
+                }
+            }
+        }
+
+        protected virtual void OnNameChanged()
+        {
+            if (NameChanged != null)
+            {
+                NameChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Tema13/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyInfo myInfo = new MyInfo();
+
+            myInfo.NameChanged += MyInfo_NameChanged;
+
+            Console.WriteLine("Введите новое имя: ");
+            string newName = Console.ReadLine();
+
+            myInfo.Name = newName;
+        }
+
+        static void MyInfo_NameChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Значение поля name было изменено.");
+        }
+    }
+}
