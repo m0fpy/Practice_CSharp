acd5e05fe674bcbf1ea300cd1f0d03222fdeb1de
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-06 10:53:06 +0300

    Сделано задание 3

---
+++ Tema15/Task3/MyListExtension.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace Task3
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public void Add(T item)
+        {
+            internalList.Add(item);
+        }
+
+        public T this[int index]
+        {
+            get { return internalList[index]; }
+        }
+
+        public int Count
+        {
+            get { return internalList.Count; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return internalList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return internalList.GetEnumerator();
+        }
+    }
+
+    public static class MyListExtensions
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] array = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array[i] = list[i];
+            }
+            return array;
+        }
+    }
+}
---
+++ Tema15/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            int[] array = myList.GetArray();
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
