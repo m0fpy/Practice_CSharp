a2e82b36675c4661b562ca1f209166c54e8caa2a
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-06 11:27:42 +0300

    Сделано задание 7

---
+++ Tema16/Task7/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allFiles = GetAllFiles(@"C:\");
+            Console.WriteLine("Список всех файлов на локальном диске:");
+            foreach (string file in allFiles)
+            {
+                Console.WriteLine(file);
+            }
+
+            string directoryName = @"D:\Example_36tp";
+            CreateDirectory(directoryName);
+            Console.WriteLine($"Каталог {directoryName} создан.");
+
+            string[] sourceFiles = GetSourceFiles(@"C:\Windows\Fonts").Take(3).ToArray();
+
+            CopyFiles(sourceFiles, directoryName);
+
+            Console.WriteLine("Все операции завершены успешно.");
+        }
+
+        static string[] GetAllFiles(string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath);
+        }
+
+        static void CreateDirectory(string directoryPath)
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        static string[] GetSourceFiles(string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath);
+        }
+
+        static void CopyFiles(string[] sourceFiles, string destinationDirectory)
+        {
+            foreach (string sourceFile in sourceFiles)
+            {
+                string destinationFile = Path.Combine(destinationDirectory, Path.GetFileName(sourceFile));
+                File.Copy(sourceFile, destinationFile);
+                SetHiddenAttribute(destinationFile);
+                CreateShortcut(destinationFile);
+            }
+        }
+
+        static void SetHiddenAttribute(string filePath)
+        {
+            File.SetAttributes(filePath, FileAttributes.Hidden);
+        }
+
+        static void CreateShortcut(string targetPath)
+        {
+            string linkFilePath = Path.ChangeExtension(targetPath, ".lnk");
+            using (StreamWriter writer = new StreamWriter(linkFilePath))
+            {
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine("URL=file:///" + targetPath.Replace('\\', '/'));
+                writer.Flush();
+            }
+        }
+    }
+}
