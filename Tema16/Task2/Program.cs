413cd5aa0d883bbfcb6d4e7aafc294ed347f1000
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-06 11:26:59 +0300

    Сделано задание 2

---
+++ Tema16/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\New_folder";
+
+            try
+            {
+                Directory.CreateDirectory(path);
+                Console.WriteLine("Папка успешно создана.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при создании папки: " + e.Message);
+            }
+        }
+    }
+}
