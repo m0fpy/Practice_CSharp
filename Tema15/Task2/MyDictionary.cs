68c338e58544166ba3e7740ca3cc0d65b3476a98
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-06 10:52:56 +0300

    Сделано задание 2

---
+++ Tema15/Task2/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private List<KeyValuePair<TKey, TValue>> pairs = new List<KeyValuePair<TKey, TValue>>();
+
+        public void Add(TKey key, TValue value)
+        {
+            pairs.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                foreach (var pair in pairs)
+                {
+                    if (pair.Key.Equals(key))
+                    {
+                        return pair.Value;
+                    }
+                }
+                throw new KeyNotFoundException($"Ключ '{key}' не найден.");
+            }
+        }
+
+        public int Count
+        {
+            get { return pairs.Count; }
+        }
+    }
+}
---
+++ Tema15/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDict = new MyDictionary<string, int>();
+
+            myDict.Add("Матвей", 1);
+            myDict.Add("Саша", 2);
+            myDict.Add("", 3);
+
+            Console.WriteLine($"Значение ключа 'Матвей': {myDict["Матвей"]}");
+            Console.WriteLine($"Кол-во элементов в словаре: {myDict.Count}");
+        }
+    }
+}
