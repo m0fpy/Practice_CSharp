32fae25efdcb247dcc68f5e2e9393d11049c98ee
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-06 10:52:45 +0300

    Сделано задание 1

---
+++ Tema15/Task1/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public void Add(T item)
+        {
+            internalList.Add(item);
+        }
+
+        public T this[int index]
+        {
+            get { return internalList[index]; }
+        }
+
+        public int Count
+        {
+            get { return internalList.Count; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return internalList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return internalList.GetEnumerator();
+        }
+    }
+}
---
+++ Tema15/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            Console.WriteLine($"Элемент с индексом 2: {myList[2]}");
+            Console.WriteLine($"Количество элементов: {myList.Count}");
+        }
+    }
+}
