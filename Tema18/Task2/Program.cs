11f01620e40004c6a33e9d780e05ad123495d7bb
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-08 09:32:35 +0300

    Сделано задание 2

---
+++ Tema18/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "input.txt";
+            string content = File.ReadAllText(filePath);
+
+            string result = ProcessText(content);
+            Console.WriteLine(result);
+        }
+
+        static string ProcessText(string text)
+        {
+            Queue<char> nonDigits = new Queue<char>();
+            Queue<char> digits = new Queue<char>();
+
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    digits.Enqueue(c);
+                }
+                else
+                {
+                    nonDigits.Enqueue(c);
+                }
+            }
+
+            string result = "";
+            while (nonDigits.Count > 0)
+            {
+                result += nonDigits.Dequeue();
+            }
+            while (digits.Count > 0)
+            {
+                result += digits.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
