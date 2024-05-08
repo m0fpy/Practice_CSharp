5c88bada749b3c3a9675ab0887467c070fb3d2af
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-08 11:36:08 +0300

    Сделано задание 13

---
+++ Tema19/Task13/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task13
+{
+    /// <summary>
+    /// Содержит логику программы для работы с файлами.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Главная точка входа в приложение.
+        /// </summary>
+        /// <param name="args">Массив аргументов командной строки.</param>
+        static void Main(string[] args)
+        {
+            string inputFileName = "input.txt";
+            string outputFileName = "output.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(inputFileName))
+                {
+                    writer.WriteLine("0101010");
+                    writer.WriteLine("110100");
+                    writer.WriteLine("001110");
+                    writer.WriteLine("101010");
+                }
+
+                Console.WriteLine($"Файл {inputFileName} создан.");
+
+                string[] lines = File.ReadAllLines(inputFileName);
+
+                using (StreamWriter writer = new StreamWriter(outputFileName))
+                {
+                    foreach (string line in lines)
+                    {
+                        string modifiedLine = line.Replace('0', '2').Replace('1', '0').Replace('2', '1');
+
+                        writer.WriteLine(modifiedLine);
+                    }
+                }
+
+                Console.WriteLine($"Замена символов выполнена. Файл {outputFileName} создан.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+        }
+    }
+}
