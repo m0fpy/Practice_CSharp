58b56a6680276a002304ea3988570ed486986a79
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-08 09:32:28 +0300

    Сделано задание 1

---
+++ Tema18/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+            string result = ProcessString(input);
+            Console.WriteLine(result);
+        }
+
+        static string ProcessString(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '#')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            char[] resultArray = stack.ToArray();
+            Array.Reverse(resultArray);
+            string result = new string(resultArray);
+            return result;
+        }
+    }
+}
