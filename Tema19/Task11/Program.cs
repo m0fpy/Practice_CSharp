457554cc73ff6ea76c47eb9757b6c84dc1b54aa9
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-08 11:35:49 +0300

    Сделано задание 11

---
+++ Tema19/Task11/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task11
+{
+    /// <summary>
+    /// Содержит логику программы для создания папки.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Главная точка входа в приложение.
+        /// </summary>
+        /// <param name="args">Массив аргументов командной строки.</param>
+        static void Main(string[] args)
+        {
+            string path = @"D:\New_folder";
+
+            try
+            {
+                Directory.CreateDirectory(path);
+                Console.WriteLine("Папка успешно создана.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при создании папки: " + e.Message);
+            }
+        }
+    }
+}
