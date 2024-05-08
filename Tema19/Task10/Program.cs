23a142f94df16c38fded4d70b7d4184aa66813af
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-08 11:35:41 +0300

    Сделано задание 10

---
+++ Tema19/Task10/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Task10
+{
+    /// <summary>
+    /// Содержит логику программы для работы с файлом чисел.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Главная точка входа в приложение.
+        /// </summary>
+        /// <param name="args">Массив аргументов командной строки.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел в файле: ");
+            int count = int.Parse(Console.ReadLine());
+
+            GenerateRandomNumbersToFile("numbers.txt", count);
+
+            int max = FindMaxNumber("numbers.txt");
+            Console.WriteLine($"Максимальное число: {max}");
+
+            int negativeCount = CountNegativeNumbers("numbers.txt");
+            Console.WriteLine($"Количество отрицательных чисел: {negativeCount}");
+        }
+
+        /// <summary>
+        /// Генерирует случайные числа и записывает их в файл.
+        /// </summary>
+        /// <param name="fileName">Имя файла для записи.</param>
+        /// <param name="count">Количество чисел для генерации.</param>
+        static void GenerateRandomNumbersToFile(string fileName, int count)
+        {
+            Random rand = new Random();
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    sw.WriteLine(rand.Next(-100, 101));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Находит максимальное число в файле.
+        /// </summary>
+        /// <param name="fileName">Имя файла с числами.</param>
+        /// <returns>Максимальное число.</returns>
+        static int FindMaxNumber(string fileName)
+        {
+            int max = int.MinValue;
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int number = int.Parse(line);
+                    if (number > max)
+                    {
+                        max = number;
+                    }
+                }
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// Подсчитывает количество отрицательных чисел в файле.
+        /// </summary>
+        /// <param name="fileName">Имя файла с числами.</param>
+        /// <returns>Количество отрицательных чисел.</returns>
+        static int CountNegativeNumbers(string fileName)
+        {
+            int count = 0;
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int number = int.Parse(line);
+                    if (number < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
