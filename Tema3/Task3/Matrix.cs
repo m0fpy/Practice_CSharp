a57ccf919073d79867c9e0918783aa9f13c8d761
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-10 10:12:35 +0300

    Revert "Сделано задание 3"
    This reverts commit 4de0a17c661e0efa946a817a41461a4c73284d05.

--- Tema3/Task3/Matrix.cs
+++ Tema3/Task3/Matrix.cs
@@ -1,4 +1,10 @@
-﻿namespace Task3
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
 {
     internal class Matrix
     {
