ccb13e7663402e57d6aa8d118ca2bec565b9d7ce
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-18 10:02:26 +0300

    Сдедано задание 2

---
+++ Tema25/Task2/About.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+            buttonClose.Click += ButtonClose_Click;
+        }
+
+        public About(string appInfo) : this()
+        {
+            textBlockInfo.Text = appInfo;
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
