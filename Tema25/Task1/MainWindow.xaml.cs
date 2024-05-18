63621527a26300ae82bc1818bf73d4a7a3984da9
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-18 10:02:17 +0300

    Сделано задание 1

---
+++ Tema25/Task1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonEnter.Click += ButtonEnter_Click;
+            buttonAbout.Click += ButtonAbout_Click;
+        }
+
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            string hello = "Hello";
+            string input = string.Empty;
+            if (string.IsNullOrEmpty(textBoxEnterName.Text) || string.IsNullOrWhiteSpace(textBoxEnterName.Text))
+            {
+                input = "World!";
+            }
+            else
+            {
+                input = textBoxEnterName.Text;
+            }
+            
+            textBlockHello.Text = hello + " " + input;
+        }
+
+        private void ButtonAbout_Click(object sender, RoutedEventArgs e)
+        {
+            string info = $"О программе {this.Title}";
+            About about = new About(info);
+            about.ShowDialog();
+        }
+    }
+}
