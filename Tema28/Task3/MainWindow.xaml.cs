574d4f19f3d29d59bf2772e3bd6d9ddcdf5a8428
m0fpy <151434381+m0fpy@users.noreply.github.com>
2024-05-21 11:56:18 +0300

    Сделано задание 3

---
+++ Tema28/Task3/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int clickCount = int.Parse(ClickCount.Text);
+            clickCount++;
+            ClickCount.Text = clickCount.ToString();
+        }
+    }
+}
